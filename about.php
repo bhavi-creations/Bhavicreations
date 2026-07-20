4c8d9c142759ab36d94887070a55d61c0d431baf
manimalladi <manimalladi05@gmail.com>
2026-07-20 17:08:27 +0530

    snathosh image remmove in website

--- about.php
+++ about.php
@@ -2001,7 +2001,7 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="carousel-item">
+                                    <!-- <div class="carousel-item">
                                         <div class="col-md-4">
                                             <div class="card card-body">
                                                 <img class="img-fluid"
@@ -2009,7 +2009,7 @@
                                                    
                                             </div>
                                         </div>
-                                    </div>
+                                    </div> -->
 
 
                                     <div class="carousel-item">
@@ -2201,7 +2201,7 @@
                     aria-labelledby="pills-Designers-kakinada-tab" tabindex="0">
                     <div class="container text-center my-3">
                         <div class="row mx-auto my-auto" style="justify-content: space-evenly;">
-                            <div class="flip-card mt-4">
+                            <!-- <div class="flip-card mt-4">
                                 <div class="flip-card-inner">
                                     <div class="flip-card-front">
                                         <img src="assests/images/Bhavi_Creations/santosh.png"
@@ -2213,7 +2213,7 @@
                                         <p>Designer</p>
                                     </div>
                                 </div>
-                            </div>
+                            </div> -->
                             <div class="flip-card  mt-4">
                                 <div class="flip-card-inner">
                                     <div class="flip-card-front">
